3be1444b2a85e9285d31ee7c3de83097e355e79a
AlxandrV <a.vigneaud@gmail.com>
2021-09-21 15:42:19 +0200

    Update - request to scrap

--- script.py
+++ script.py
@@ -1,19 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
+"""
+Funtion scrap to url
+
+url to scrap
+param @url
+
+query to find in content
+param @q_find
+
+return @array
+"""
+def scrap(url, q_find):
+    r = requests.get(url)
+    page = r.content
+
+    soup = BeautifulSoup(page, "html.parser")
+
+    nodes = q_find
+
+    array = []
+    for node in nodes:
+        node_str = node.text
+        node_str = node_str.replace('\n', '')
+        array.append(node_str.strip())
+
+    return array
+
+"""Category"""
 url = "http://books.toscrape.com/"
-r = requests.get(url)
-page = r.content
+query = soup.find("ul", class_="nav nav-list").find("li").find_all("a")
+categorys = scrap(url, query)
 
-soup = BeautifulSoup(page, "html.parser")
-
-nodes = soup.find("ul", class_="nav nav-list").find("li").find_all("a")
-
-# print(nodes.string)
-array = []
-for node in nodes:
-    node_str = node.text
-    node_str = node_str.replace('\n', '')
-    array.append(node_str.strip())
-
-print(array)
+print(categorys)
