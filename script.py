a6e103b41a72984e91cf7fea49d5217bec389a73
AlxandrV <a.vigneaud@gmail.com>
2021-09-21 15:50:22 +0200

    Update - define function scrap, return soup

--- script.py
+++ script.py
@@ -1,36 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 """
 Funtion scrap to url
 
 url to scrap
 param @url
 
-query to find in content
-param @q_find
-
-return @array
+return content html
+return @Beautiful object
 """
-def scrap(url, q_find):
+def scrap(url):
+    url = "http://books.toscrape.com/"
     r = requests.get(url)
     page = r.content
 
-    soup = BeautifulSoup(page, "html.parser")
+    return BeautifulSoup(page, "html.parser")
 
-    nodes = q_find
-
-    array = []
-    for node in nodes:
-        node_str = node.text
-        node_str = node_str.replace('\n', '')
-        array.append(node_str.strip())
-
-    return array
 
 """Category"""
 url = "http://books.toscrape.com/"
-query = soup.find("ul", class_="nav nav-list").find("li").find_all("a")
-categorys = scrap(url, query)
+soup = scrap(url)
+nodes = soup.find("ul", class_="nav nav-list").find("li").find_all("a")
 
-print(categorys)
+array = []
+for node in nodes:
+    node_str = node.text
+    node_str = node_str.replace('\n', '')
+    array.append(node_str.strip())
+
+print(array)
