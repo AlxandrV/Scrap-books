993db8c93c909cb0d50913c6629ead26e8916651
AlxandrV <a.vigneaud@gmail.com>
2021-09-22 20:45:40 +0200

    Update - Add pagination

--- script.py
+++ script.py
@@ -16,34 +16,68 @@ def scrap(url):
 
     if r.ok:
         page = r.content
-        return BeautifulSoup(page, "html.parser")
+        return BeautifulSoup(page, 'html.parser')
     else:
         return r.status_code
 
 
+"""
+Function strip a string
+
+string to strip
+param @string
+
+Return strip to string
+return @string
+"""
+def strip_string(string):
+    string = string.text
+    return string.replace('\n', '').strip()
+
+
+
 """Extract categorys"""
-url = "http://books.toscrape.com/"
+url = 'http://books.toscrape.com/'
 soup = scrap(url)
-categorys = soup.find("ul", class_="nav nav-list").find("li").find_all("a")
+categorys = soup.find('ul', class_='nav nav-list').find('li').find_all('a')
 
 category_name = []
 book_url = []
 
 """Foreach category"""
 for category in categorys:
-    category_str = category.text
-    category_str = category_str.replace('\n', '').strip()
+    # Get category name
+    category_str = strip_string(category)
+    # category_str = category_str.replace('\n', '').strip()
     category_name.append(category_str)
 
     # Get url category
     url_category = url + category.get('href')
     url_category = url_category.replace('index.html', '')
     soup_category = scrap(url_category)
-    books = soup_category.find_all('div', class_="image_container")
 
-    """Foreach book"""
-    for book in books:
-        book = book.find('a').get('href')
-        book_url.append(book)
+    # Get page number
+    page = soup_category.find('li', class_='current')
+    if page != None:
+        page = strip_string(page)
+        page = int(page[-2:].strip())
+
+        """Foreach page"""
+        i = 1
+        while i <= page:
+            soup_page = scrap(url_category + f'page-{i}.html')
+            print(soup_page)
+            i += 1
+
+
+
+
+
+    # books = soup_category.find_all('div', class_="image_container")
+
+    # """Foreach book"""
+    # for book in books:
+    #     book = book.find('a').get('href')
+    #     book_url.append(book)
 
 print(len(book_url))
